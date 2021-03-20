b497595c34ed0e871b44300cbadf5a90d00868be
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-03-20 17:49:24 +0200

    working on Rest api

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 
+const bodyparser = require("body-parser");
+
 const feedRoutes = require("./routes/feed");
 
 const app = express();
+
+// app.use(bodyparser.urlencoded());
+app.use(bodyparser.json());
 
 app.use("/feed", feedRoutes);
 
